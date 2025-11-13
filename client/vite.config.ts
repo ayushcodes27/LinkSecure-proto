5879cf12c7ecc7672ef3d25ae77c18428fe07c6e
Ayush <deoreayush555@gmail.com>
2025-11-13 19:16:13 +0530

    Configure short links and environment variables for production deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
@@ -23,4 +23,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
