94ce0d744913e0bdbcd01c221ee65169772cfcea
Ayush <deoreayush555@gmail.com>
2025-11-14 22:03:56 +0530

    Fix CORS to allow production frontend origin

--- server/server.ts
+++ server/server.ts
@@ -19,7 +19,15 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:8080',
+    'https://linksecure-2cdc.onrender.com'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
